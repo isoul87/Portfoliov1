f0843f3f83f3e9a83ce93c429c0d58d867f92d7d
isoul87 <atbrxnk@gmail.com>
2026-03-24 20:52:49 +0700

    fix text about me

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,10 +6,10 @@ export const About = () => {
   const [isExpanded, setIsExpanded] = useState(false);
 
   // Bagian teks yang selalu muncul (intro)
-  const introText = "A 12th-grade student at SMA Wahidiyah Kepanjen with a deep passion for digital architecture. I specialize in Fullstack Development, focusing on creating robust and scalable web applications. My journey is driven by a commitment to continuous learning and a mission to transform complex ideas into functional, high-performance digital solutions.";
+  const introText = "A 12th-grade student at SMA Wahidiyah Kepanjen with a deep passion for digital architecture. I specialize in Fullstack Development, ";
   
   // Bagian teks yang disembunyikan
-  const hiddenText = " I am mastering SEO strategies and crafting seamless UI/UX experiences. My goal? Bridging the gap between creative design and high-performance code to create digital products that actually work.";
+  const hiddenText = " focusing on creating robust and scalable web applications. My journey is driven by a commitment to continuous learning and a mission to transform complex ideas into functional, high-performance digital solutions.";
 
   return (
     <section className="flex items-center justify-center bg-[#f8f8f8]">
