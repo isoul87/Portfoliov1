f09d10558bde595c9712a1f191793c978d9091e8
isoul87 <atbrxnk@gmail.com>
2026-03-30 13:09:04 +0700

    change loader

--- src/Components/GreetingLoader.jsx
+++ src/Components/GreetingLoader.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'motion/react';
+import { useState, useEffect } from 'react';
+import { motion } from 'motion/react';
 
 const words = [
   "Hello everyone",            
